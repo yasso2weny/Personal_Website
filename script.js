44e5133d70d5ada6316029241f1a50c69555d8d8
YassinMohamed <52466287+yasso2weny@users.noreply.github.com>
2024-03-06 10:41:25 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,64 @@
-const slider = document.getElementById('projects-section'); // Get the slider container
-  let isDown = false;
-  let startX;
-  let scrollLeft;
+const slider = document.getElementById("projects-section"); // Get the slider container
+let isDown = false;
+let startX;
+let scrollLeft;
 
-  slider.addEventListener('mousedown', (e) => {
-    isDown = true;
-    slider.classList.add('active');
-    startX = e.pageX - slider.offsetLeft;
-    scrollLeft = slider.scrollLeft;
-  });
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  slider.classList.add("active");
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
 
-  slider.addEventListener('mouseleave', () => {
-    isDown = false;
-    slider.classList.remove('active');
-  });
+slider.addEventListener("mouseleave", () => {
+  isDown = false;
+  slider.classList.remove("active");
+});
 
-  slider.addEventListener('mouseup', () => {
-    isDown = false;
-    slider.classList.remove('active');
-  });
+slider.addEventListener("mouseup", () => {
+  isDown = false;
+  slider.classList.remove("active");
+});
 
-  slider.addEventListener('mousemove', (e) => {
-    if (!isDown) return; // stop the fn from running
-    e.preventDefault();
-    const x = e.pageX - slider.offsetLeft;
-    const walk = (x - startX) * 3; //scroll-fast
-    slider.scrollLeft = scrollLeft - walk;
-  });
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return; // stop the fn from running
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 3; //scroll-fast
+  slider.scrollLeft = scrollLeft - walk;
+});
 
+// Navbar
+const openNavBar = document.querySelector("#burger-img");
+const closeNavBar = document.querySelector("#close-img");
+const links = document.querySelector(".mbl-container");
+
+closeNavBar.addEventListener("click", function () {
+  links.classList.add("hide-links");
+});
+
+openNavBar.addEventListener("click", function () {
+  links.classList.remove("hide-links");
+});
+
+// My SKills
+
+const about = document.querySelector(".about");
+const btns = document.querySelectorAll(".tab-btn");
+const articles = document.querySelectorAll(".content");
+about.addEventListener("click", function (e) {
+  const id = e.target.dataset.id;
+  if (id) {
+    // remove selected from other buttons
+    btns.forEach(function (btn) {
+      btn.classList.remove("active");
+    });
+    e.target.classList.add("active");
+    // hide other articles
+    articles.forEach(function (article) {
+      article.classList.remove("active");
+    });
+    const element = document.getElementById(id);
+    element.classList.add("active");
+  }
+});
