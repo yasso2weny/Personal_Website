0a7b4b7d737beac7c55b82e1204c1672c2bef604
YassinMohamed <52466287+yasso2weny@users.noreply.github.com>
2024-03-29 20:53:58 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,7 @@
-const slider = document.getElementById("projects-section"); // Get the slider container
-let isDown = false;
-let startX;
-let scrollLeft;
+//================
+// Navigation Bar
+//================
 
-slider.addEventListener("mousedown", (e) => {
-  isDown = true;
-  slider.classList.add("active");
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
-});
-
-slider.addEventListener("mouseleave", () => {
-  isDown = false;
-  slider.classList.remove("active");
-});
-
-slider.addEventListener("mouseup", () => {
-  isDown = false;
-  slider.classList.remove("active");
-});
-
-slider.addEventListener("mousemove", (e) => {
-  if (!isDown) return; // stop the fn from running
-  e.preventDefault();
-  const x = e.pageX - slider.offsetLeft;
-  const walk = (x - startX) * 3; //scroll-fast
-  slider.scrollLeft = scrollLeft - walk;
-});
-
-// Navbar
 const openNavBar = document.querySelector("#burger-img");
 const closeNavBar = document.querySelector("#close-img");
 const links = document.querySelector(".mbl-container");
@@ -41,7 +14,9 @@ openNavBar.addEventListener("click", function () {
   links.classList.remove("hide-links");
 });
 
-// My SKills
+//================
+// Skills Section
+//================
 
 const about = document.querySelector(".about");
 const btns = document.querySelectorAll(".tab-btn");
@@ -61,4 +36,93 @@ about.addEventListener("click", function (e) {
     const element = document.getElementById(id);
     element.classList.add("active");
   }
+});
+
+//================
+// Project Section
+//================
+
+// local reviews data
+const reviews = [
+  {
+    id: 1,
+    prjtitle: "Tip Calculator",
+    img: "images/project1.jpg",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ab nam aliquid aperiam atque minima illum nesciunt neque dignissimos voluptate!",
+  },
+  {
+    id: 2,
+    prjtitle: "Product Card",
+    img: "images/project2.jpg",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ab nam aliquid aperiam atque minima illum nesciunt neque dignissimos voluptate!",
+  },
+  {
+    id: 3,
+    prjtitle: "Newsletter Sign Up",
+    img: "images/project3.jpg",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ab nam aliquid aperiam atque minima illum nesciunt neque dignissimos voluptate!",
+  },
+  {
+    id: 4,
+    prjtitle: "Pricing Component",
+    img: "images/project4.jpg",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ab nam aliquid aperiam atque minima illum nesciunt neque dignissimos voluptate!",
+  },
+  {
+    id: 5,
+    prjtitle: "FAQ Accordion",
+    img: "images/project5.jpg",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ab nam aliquid aperiam atque minima illum nesciunt neque dignissimos voluptate!",
+  },
+  {
+    id: 6,
+    prjtitle: "Social Media Dashboard",
+    img: "images/project6.jpg",
+    text: " A dashboard design that displays social media metrics at a glance. It includes charts, follower counts, and engagement statistics. This project highlights my ability to work with data visualization libraries and create informative, visually appealing interfaces that are both functional and user-friendly.",
+  },
+];
+// select items
+const img = document.getElementById("prjimg");
+const author = document.getElementById("prjtitle");
+
+const info = document.getElementById("info");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+
+// set starting item
+let currentItem = 0;
+
+// load initial item
+window.addEventListener("DOMContentLoaded", function () {
+  const item = reviews[currentItem];
+  img.src = item.img;
+  author.textContent = item.prjtitle;
+
+  info.textContent = item.text;
+});
+
+// show person based on item
+function showPerson(person) {
+  const item = reviews[person];
+  img.src = item.img;
+  author.textContent = item.prjtitle;
+
+  info.textContent = item.text;
+}
+// show next person
+nextBtn.addEventListener("click", function () {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  showPerson(currentItem);
+});
+// show prev person
+prevBtn.addEventListener("click", function () {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson(currentItem);
 });
